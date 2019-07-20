1b05e9c49fea97233b9552ad299dd44729b6fa66
Cody L. Robertson <unlinkedcody@yahoo.com>
2019-07-20 03:41:40 -0500

    Continued Chapter (1) Data Types
    Continued Chapter (1) Data Types

--- AutomateTheBoringStuff.py
+++ AutomateTheBoringStuff.py
@@ -1,8 +1,14 @@
 #AutomateTheBoringStuff
+
+#Test Print HelloWorld as per tradition
 print("HelloWorld")
 print("")
+print("------------------------------------------------------------------------------------------------------------------------")
+
+#Chapter (1) Operators and Precedence
 print("CHAPTER (1)")
 print("")
+print("------------------------------------------------------------------------------------------------------------------------")
 print("Below you will see examples of Python Expressions")
 print("** = Exponent (2**3 = 8)")
 print("% = Modulus/remainder (22 % 8 = 6)")
@@ -12,6 +18,21 @@ print("* = Multiplication (3 * 5 = 15)")
 print("- = Subtraction(5 - 2 = 3)")
 print("+ = Addition(2 + 2 = 4)")
 print("")
+print("------------------------------------------------------------------------------------------------------------------------")
 print('Order of operations is called "Precedence" and goes as follows from left to right:')
 print("**,*,/,//,%, +,-")
 print("You may use parenthesis to override read order.")
+print("")
+print("------------------------------------------------------------------------------------------------------------------------")
+
+#Chapter (1) The Integer, Floating-Point, and String Data Types
+print("There are three (3) types of Data Types; Integers, Floating-Points, and Strings.")
+print("This is an integer:")
+print(4)
+print("This is a Floating-Point:")
+print(0.63)
+print("This is a string:")
+print("ABCDEFG")
+
+#Chapter (1) String Concatenation and Replication
+print("TO BE CONTINUED")
