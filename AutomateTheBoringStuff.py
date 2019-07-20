3c66c849ee2877a640cbd39fea7c45155b1d2b54
Cody L. Robertson <unlinkedcody@yahoo.com>
2019-07-20 11:08:46 -0500

    Chapter (2) + Custom Code
    Dabbled in Chapter (2) as well as messed around with the different functionalities of the learned code.

--- AutomateTheBoringStuff.py
+++ AutomateTheBoringStuff.py
@@ -1,3 +1,8 @@
+
+import time
+
+
+
 #AutomateTheBoringStuff
 
 #Test Print HelloWorld as per tradition
@@ -35,4 +40,87 @@ print("This is a string:")
 print("ABCDEFG")
 
 #Chapter (1) String Concatenation and Replication
-print("TO BE CONTINUED")
+
+print("")
+print("Chapter 2")
+print("")
+#Chapter (2) Flow and Conditionals
+True and True
+
+name = str(input("What is your name?   "))
+password = str(input("What is the password?   "))
+
+if name == 'Cody':
+	print('Hello Cody')
+if password =='PythonPassword':
+	print("Access Granted.")
+else:
+	print("Password Incorrect.")
+
+spam = 0
+while spam < 5:
+	print('Hello World.')
+	spam = spam +1
+	ham = 0
+	while ham <5:
+		print("Add Ham!")
+		ham = ham +1
+		cheese = 0
+		while cheese <5:
+			print("Adding Cheese!")
+			cheese = cheese +1
+
+#This section of code works, but don't run it, lest you crash everything you hold dear.....
+'''year = 0
+while year < 10:
+	print ("It is year " + str(year))
+	year = year + 1
+	month = 0
+	while month < 12:
+		print("It is month "+ str(month))
+		month = month + 1
+		week = 0
+		while week < 4:
+			print("It is week " +str(week))
+			week = week + 1
+			day = 0
+			while day < 7:
+				print("It is day " + str(day))
+				day = day + 1
+				hour = 0
+				while hour < 24:
+					print("It is hour " + str(hour))
+					hour = hour + 1
+					minute = 0
+					while minute < 60:
+						print("It is minute " + str(minute))
+						minute = minute + 1
+						second = 0
+						while second < 60:
+							print("It is second "+str(second))
+							second = second + 1
+
+							'''
+name2 = ''
+while name2 != name:
+	print('Please type your name.')
+	name2 = input()
+print("Thanks!")
+
+while True:
+	print("Please type your name yet again.")
+	name3 = input()
+	if name3 == name2 or name:
+		break
+print("Thank you again!")
+
+while True:
+	counter = 0
+	print(str(counter))
+	time.sleep(1)
+	counter = counter + 1
+	if counter == 20:
+		break
+print("Done!")
+
+
